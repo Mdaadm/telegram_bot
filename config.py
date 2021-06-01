66b3cf2809697ee52ab05757573f464eb318516a
mdaadm <mdazhidkov@gmail.com>
2021-06-01 22:39:01 +0300

    updated config

--- config.py
+++ config.py
@@ -1,55 +1,42 @@
-#!/usr/bin/env python3
+fb_access_token = "token"
 
-# Facebook token
-fb_access_token = 'token'
+sheets_clients_token = 'token'
+sheets_fin_token = 'token'
 
-# Facebook verification token
-fb_verify_token = 'token'
-
-# Vkontakte token
 token_vk = 'token'
 
-# Telegram bot token
 tg_token = 'token'
 
-# Credentials file for google sheets api
-cred_final = 'path'
+cred_final = "/root/botenv/bot/credentials_final.json"
 
-# Facebook group id
-fb_group_id = 'id'
+fb_group_id = '1'
 
-# base.csv file path (for payment recording)
-base_file = 'path'
+base_file = "/var/www/base.csv"
 
-# Path to folder for saved files
-files_path = 'path'
+files_path = "/root/botenv/bot/files/"
 
-# Path to folder for saved payment screenshots
-pay_imgs_path = 'path'
+pay_imgs_path = "/root/botenv/bot/pay_imgs/"
 
-# Telegram support chat id
-group_id = 1
+group_id = -1
 
-# Id of pinned message in TG support group
-pinned_msg = 1
+pinned_msg = 45686
 
-# TG bot id
 bot_id = 1
 
-# Token for telegram payment (Юкасса)
 pay_token = 'token'
 
-# Free OCR api token (https://ocr.space/)
 ocr_token = 'token'
 
-# Path to database file
-db_file = 'path'
+fb_verify_token = "token<"
 
-# Developer id
+db_file = 'base.db'
+
 my_id = 1
-
-# Owner id
 owner_id = 1
 
-# TG bot name
-bot_name = '@name'
+bot_name = "@name"
+
+review_link = 'https://yandex.ru/maps/org/vpn_kitayskiy_uksus/202625555152/reviews'
+
+ssl_context1 = '/etc/letsencrypt/live/zgc.su/fullchain.pem'
+ssl_context2 = '/etc/letsencrypt/live/zgc.su/privkey.pem'
